2287f34589408f83aa4b67ab2ef9cc65324bd6ff
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2023-01-16 19:28:21 +0530

    Contact responsive fix

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -18,31 +18,29 @@ const Contact = () => {
       <div className="max-w-[1200px] mt-[200px]  mx-auto w-full flex flex-col justify-start">
         <div className="mx-auto h-[400px] flex flex-col select-none text-[#A9A9A9]">
           <div className="flex justify-around text-center">
-            <p className=" text-sm w-[160px] mx-4 mt-5">
+            <p className=" text-[8px] sm:text-xs md:text-sm w-[160px] mx-4 mt-5">
               You looking for a good developer
             </p>
-            <p className="  text-sm w-[150px] mx-4 mt-5">
+            <p className=" text-[8px] sm:text-xs md:text-sm w-[150px] mx-4 mt-5">
               Me looking for a good job
             </p>
           </div>
           <img
             src={Meme}
             alt="spiderman-meme"
-            className=" "
-            width={450}
-            height={450}
+            className="m-auto w-[350px] h-[300px] sm:w-[450px] sm:h-[450px] "
           />
         </div>
-        <div className="flex flex-col justify-center text-center">
+        <div className="text-xs space-y-2 md:text-base lg:text-lg flex flex-col justify-center text-center">
           <Typed
-            className="text-lg text-white select-custom "
+            className="text-white select-custom "
             strings={["Let's get in touch"]}
             typeSpeed={35}
             showCursor={false}
             onStop={() => {}}
           />
           <Typed
-            className="text-lg text-white select-custom "
+            className="text-white select-custom "
             strings={["Feel free to slide into my dms"]}
             typeSpeed={35}
             showCursor={false}
@@ -50,7 +48,7 @@ const Contact = () => {
             onStop={() => {}}
           />
           <Typed
-            className="text-lg px-2  select-custom-last about-hero-last"
+            className="= px-2  select-custom-last about-hero-last"
             strings={[";)"]}
             typeSpeed={70}
             showCursor={false}
@@ -61,11 +59,11 @@ const Contact = () => {
           onClick={() => {
             window.location = "mailto:contact@gabrielaxy.com";
           }}
-          className="mt-[90px] h-[75px] w-[80%] lg:w-[50%]   bg-[#D22B2B] mx-auto rounded-2xl cursor-pointer shadow-md fill-white hover:fill-black shadow-red-500/50  hover:shadow-white transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-103 hover:bg-white hover:text-[#000000] duration-300"
+          className="mt-[90px] h-[65px] sm:h-[75px] sm:w-[80%] lg:w-[50%]   bg-[#D22B2B] mx-auto rounded-2xl cursor-pointer shadow-md fill-white hover:fill-black shadow-red-500/50  hover:shadow-white transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-103 hover:bg-white hover:text-[#000000] duration-300"
         >
           <div className="w-[100%] h-[100%]  flex align-middle justify-center select-none">
-            <MailIcon className="fill-inherit  w-[30px] hover:fill-inherit  hover:cursor-pointer mx-3 my-auto" />
-            <p className="inline text-center text-sm mx-3 h-auto my-auto">
+            <MailIcon className="fill-inherit w-[25px] sm:w-[30px] hover:fill-inherit  hover:cursor-pointer mx-3 my-auto" />
+            <p className="inline text-center text-[10px] sm:text-sm mx-3 h-auto my-auto">
               contact@gabrielaxy.com
             </p>
           </div>
